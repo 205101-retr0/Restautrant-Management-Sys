cc1bfa38640541b481da1c4958398de8004a98b1
205101-ascharan <57037826+205101-retr0@users.noreply.github.com>
2021-11-26 12:22:44 +0530

    making bill logic flawed

--- orders.py
+++ orders.py
@@ -2,23 +2,24 @@ from flask import Flask, Blueprint, render_template, request, redirect, url_for,
 
 orders = Blueprint('orders', __name__)
 
-@orders.route('/bill', methods=['GET', 'POST'])
+@orders.route('/bill', methods=['POST'])
 def billing():
     if request.method == 'POST':
         items = request.form.getlist('items')
         quantity = request.form.getlist('item-quantity')
         print(items, quantity)
+        i=0
         ## Make a dictionary of all the orders, their quantities and their prices from items
         dict_to_send ={}
-        # for item in items:
-        #     menu = item.split('_')[0]
-        #     price = item.split('_')[1]
-        #     dict_to_send[menu]=price
-            
-        items = {
-            "name" : "tapiceria",
-            "price":   "120",
-            "quantity": "1",
-        }
         
-    return render_template('bill.html', items=items)
+        ## Figure out the logic here
+        
+        for i, j in zip(items, quantity):
+            # print(i)
+            if j == '':
+                continue
+            else:
+                dict_to_send[i] = int(j)
+        print(dict_to_send)
+        
+    return render_template('bill.html', items=dict_to_send)
